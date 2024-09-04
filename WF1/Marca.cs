2f81ebaeacb8e4d3f659cbaa55a602edc500e934
tamicaa-mat <tamicaa.mat@gmail.com>
2024-09-04 13:35:29 -0300

    clase marca

---
+++ WF1/Marca.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WF1
+{
+    internal class Marca
+    {
+        private int idMarca;
+        private string nombreMarca;
+
+
+        public void setidMarca(int mar)
+        {
+            idMarca = mar;
+        }
+        public int getidMarca()
+        {
+            return idMarca;
+        }
+        public void setnombreMarca(string nombreMar)
+        {
+            nombreMarca = nombreMar;
+        }
+        public string getnombreMarca()
+        {
+            return nombreMarca;
+        }
+
+
+    }
+}
