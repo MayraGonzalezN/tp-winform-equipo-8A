65e9947f83a12f62cbafbdb05e57a7221d600a4f
tamicaa-mat <tamicaa.mat@gmail.com>
2024-09-04 13:40:49 -0300

    clases

--- WF1/Articulo.cs
+++ WF1/Articulo.cs
@@ -8,5 +8,54 @@ namespace WF1
 {
     internal class Articulo
     {
+        private char codigoArticulo;
+        private string nombre;
+        private string descripcion;
+        private float precio;
+        private string imagen;// para cargar la url
+        private Marca marca;
+        private Categoria categoria;
+
+        public void setcodigoArticulo(char art)
+        {
+            codigoArticulo = art;
+        }
+        public char getcodigoArticulo()
+        {
+            return codigoArticulo;
+        }
+        public void setnombre(string nom)
+        {
+            nombre = nom;
+        }
+        public string getnombre()
+        {
+            return nombre;
+        }
+        public void setdescripcion(string desc)
+        {
+            descripcion = desc;
+        }
+        public string getdescripcion()
+        {
+            return descripcion;
+        }
+        public void setprecio(float prec)
+        {
+            precio = prec;
+        }
+        public float getprecio()
+        {
+            return precio;
+        }
+        public void setimagen(string imag)
+        {
+            imagen = imag;
+        }
+        public string getimagen()
+        {
+            return imagen;
+        }
+
     }
 }
--- WF1/Categoria.cs
+++ WF1/Categoria.cs
@@ -8,5 +8,26 @@ namespace WF1
 {
     internal class Categoria
     {
+
+        private int idCategoria;
+        private string nombreCategoria;
+
+
+        public void setidCategoria(int cat)
+        {
+            idCategoria = cat;
+        }
+        public int getidCategoria()
+        {
+            return idCategoria;
+        }
+        public void setnombreCategoria(string nombreCat)
+        {
+            nombreCategoria = nombreCat;
+        }
+        public string getnombreCategoria()
+        {
+            return nombreCategoria;
+        }
     }
 }
